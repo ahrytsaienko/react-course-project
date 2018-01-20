31fe1017b05fe3165d787d64b0211ed5f5b0f0b7
A.Hrytsaienko <temsan89@gmail.com>
2018-01-20 07:49:58 +0200

    insert first elements

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -4,10 +4,56 @@ console.log('app.js is running');
 
 // JSX - javascript XML
 var template = React.createElement(
-  'h1',
-  null,
-  'Indecision App'
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        'Indecision App'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Some info'
+    ),
+    React.createElement(
+        'ol',
+        null,
+        React.createElement(
+            'li',
+            null,
+            'Item one'
+        ),
+        React.createElement(
+            'li',
+            null,
+            'Item two'
+        )
+    )
 );
+
+var someTemplate = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        'Some element'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'test1'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'test2'
+    )
+);
+
 var appRoot = document.getElementById('app');
+var secondApp = document.getElementById('app_second');
 
 ReactDOM.render(template, appRoot);
+ReactDOM.render(someTemplate, secondApp);
--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,7 +1,27 @@
 console.log('app.js is running')
 
 // JSX - javascript XML
-let template = <h1>Indecision App</h1>
+let template = (
+    <div>
+        <h1>Indecision App</h1>
+        <p>Some info</p>
+        <ol>
+            <li>Item one</li>
+            <li>Item two</li>
+        </ol>
+    </div>
+);
+
+let someTemplate = (
+    <div>
+        <h1>Some element</h1>
+        <p>test1</p>
+        <p>test2</p>
+    </div>
+);
+
 let appRoot = document.getElementById('app');
+let secondApp = document.getElementById('app_second');
 
 ReactDOM.render(template, appRoot);
+ReactDOM.render(someTemplate, secondApp);
