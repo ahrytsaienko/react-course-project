5d1ac2fbc2367da32d21c1fca8a1c4f33abfeeb3
A.Hrytsaienko <temsan89@gmail.com>
2018-01-28 19:46:34 +0200

    render count

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -1,105 +1,60 @@
 'use strict';
 
-var head = {
-    title: 'Indecision app',
-    subtitle: 'Subtitle example'
-};
-
-var main = React.createElement(
-    'div',
-    null,
-    React.createElement(
-        'h1',
-        null,
-        head.title
-    ),
-    get_header(head.subtitle),
-    head.subtitle ? head.subtitle : 'No subtitle'
-);
-
-function get_header(subtitle) {
-    if (subtitle) {
-        return React.createElement(
-            'h2',
-            null,
-            subtitle
-        );
-    }
-};
-
-var user = {
-    name: 'John',
-    age: 26,
-    location: 'Philadelphia'
-};
-
-function get_location(location) {
-    if (location) {
-        return React.createElement(
-            'p',
-            null,
-            'Location: ',
-            location
-        );
-    }
-};
-
 var count = 0;
+
 var addOne = function addOne() {
-    console.log("click here");
+    count++;
+    showCount();
+    renderCount();
 };
 
-var fnBtnOne = function fnBtnOne() {
-    console.log("TheOne");
+var minusOne = function minusOne() {
+    count--;
+    showCount();
+    renderCount();
 };
-var fnBtnTwo = function fnBtnTwo() {
-    return console.log("TheTwoo");
+
+var reset = function reset() {
+    count = 0;
+    showCount();
+    renderCount();
 };
 
-// const templateTwo = (
-//     // <div>
-//     //     <h1> {user.name ? user.name : 'Anonymous'} </h1>
-//     //     {(user.age && user.age >= 18) && <p>Age{user.age}</p>}
-//     //     {get_location(user.location)}
-//     //</div>
+var showCount = function showCount() {
+    console.log('count: ' + count);
+};
 
-//     <div>
-//         <h1>Count: {count}</h1>
-//         <button onClick={addOne}>+1</button>
-//     </div>
-
-// );
-
-var templatebtnOne = React.createElement(
-    'div',
-    null,
-    React.createElement(
-        'button',
-        { onClick: fnBtnOne },
-        'First button'
-    ),
-    React.createElement(
-        'button',
-        { onClick: fnBtnTwo },
-        'Second button'
-    )
-);
-
-var templateTwo = React.createElement(
-    'div',
-    null,
-    React.createElement(
-        'button',
-        { onClick: fnBtnTwo },
-        'Second button'
-    )
-);
-
-var header = document.getElementById('header');
-var app = document.getElementById('app');
 var btnOne = document.getElementById('btnOne');
-var btnTwo = document.getElementById('btnTwo');
 
-ReactDOM.render(main, header);
-ReactDOM.render(templatebtnOne, btnOne);
-ReactDOM.render(templateTwo, btnTwo);
+var renderCount = function renderCount() {
+
+    var templatebtnOne = React.createElement(
+        'div',
+        null,
+        React.createElement(
+            'h1',
+            null,
+            'Count: ',
+            count
+        ),
+        React.createElement(
+            'button',
+            { onClick: addOne },
+            'First button'
+        ),
+        React.createElement(
+            'button',
+            { onClick: minusOne },
+            'Second button'
+        ),
+        React.createElement(
+            'button',
+            { onClick: reset },
+            'Reset button'
+        )
+    );
+
+    ReactDOM.render(templatebtnOne, btnOne);
+};
+
+renderCount();
--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,76 +1,44 @@
-let head = {
-    title: 'Indecision app',
-    subtitle: 'Subtitle example',
-}
-
-let main = (
-    <div>
-        <h1>{head.title}</h1>
-        {get_header(head.subtitle)}
-        {(head.subtitle ? head.subtitle : 'No subtitle')}
-    </div>
-);
-
-function get_header(subtitle) {
-    if (subtitle) {
-        return <h2>{subtitle}</h2>
-    }
-};
-
-var user = {
-    name: 'John',
-    age: 26,
-    location: 'Philadelphia'
-};
-
-function get_location(location) {
-    if (location) {
-        return <p>Location: {location}</p>;
-    }
-};
 
 let count = 0;
+
 const addOne = () => {
-    console.log("click here");
+    count++;
+    showCount();
+    renderCount();
 };
 
-const fnBtnOne = () => { console.log("TheOne") };
-const fnBtnTwo = () => console.log("TheTwoo");
+const minusOne = () => {
+    count--;
+    showCount()
+    renderCount();
+};
 
-// const templateTwo = (
-//     // <div>
-//     //     <h1> {user.name ? user.name : 'Anonymous'} </h1>
-//     //     {(user.age && user.age >= 18) && <p>Age{user.age}</p>}
-//     //     {get_location(user.location)}
-//     //</div>
+const reset = () => {
+    count = 0;
+    showCount();
+    renderCount();
+};
 
-//     <div>
-//         <h1>Count: {count}</h1>
-//         <button onClick={addOne}>+1</button>
-//     </div>
+const showCount = () => {
+    console.log(`count: ${count}`);
+};
 
-// );
-
-const templatebtnOne = (
-    <div>
-        <button onClick={fnBtnOne}>First button</button>
-        <button onClick={fnBtnTwo}>Second button</button>
-        </div>
-);
-
-const templateTwo = (
-    <div>
-    <button onClick={fnBtnTwo}>Second button</button>    
-    </div>
-);
-
-
-const header = document.getElementById('header');
-const app = document.getElementById('app');
 const btnOne = document.getElementById('btnOne');
-const btnTwo = document.getElementById('btnTwo');
 
 
-ReactDOM.render(main, header);
-ReactDOM.render(templatebtnOne, btnOne);
-ReactDOM.render(templateTwo, btnTwo)
+const renderCount = () => {
+
+    const templatebtnOne = (
+        <div>
+            <h1>Count: {count}</h1>
+            <button onClick={addOne}>First button</button>
+            <button onClick={minusOne}>Second button</button>
+            <button onClick={reset}>Reset button</button>
+        </div>
+    );
+
+    ReactDOM.render(templatebtnOne, btnOne);
+
+};
+
+renderCount();
