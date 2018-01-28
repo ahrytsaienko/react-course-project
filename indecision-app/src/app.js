50bc66413ef108a0c109613f3bf9f67a6282748f
A.Hrytsaienko <temsan89@gmail.com>
2018-01-28 20:27:55 +0200

    render form

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,20 +1,54 @@
 const app = {
     title: 'Indecision App',
     subtitle: 'Put your life in the hands of computer',
-    options: ['One', 'Two']
+    options: []
 };
 
-const template = (
-    <div>
-        <h1>{app.title}</h1>
-        {app.subtitle && <p>{app.subtitle}</p>}
-        <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
-        <ol>
-            <li>Item one</li>
-            <li>Item two</li>
-        </ol>
-    </div>
-);
+const onFormSubmit = (e) => {
+    e.preventDefault();
+
+    const option = e.target.elements.option.value;
+
+    if (option) {
+        app.options.push(option);
+        e.target.elements.option.value = '';
+    };
+    render();
+};
+
+const removeAll = (e) => {
+    app.options = [];
+    render();
+};
 
 const appRoot = document.getElementById('app');
-ReactDOM.render(template, appRoot);
+
+const render = () => {
+
+    const template = (
+        <div>
+            <h1>{app.title}</h1>
+            {app.subtitle && <p>{app.subtitle}</p>}
+            <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
+            <p>{app.options.length}</p>
+            <ol>
+                <li>Item one</li>
+                <li>Item two</li>
+            </ol>
+            <form onSubmit={onFormSubmit}>
+                <input type="text" name="option" />
+                <button>Add option</button>
+            </form>
+            <button onClick={removeAll}>Remove all</button>
+        </div>
+    );
+
+    ReactDOM.render(template, appRoot);
+
+};
+
+render();
+
+
+
+
