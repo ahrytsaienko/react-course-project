06013f13a440ff22ffc5ab8e7fc19a9a7ffb4e75
A.Hrytsaienko <temsan89@gmail.com>
2018-01-29 19:49:45 +0200

    new component

--- indecision-app/playground/es6-classes-1.js
+++ indecision-app/playground/es6-classes-1.js
@@ -1,12 +1,53 @@
 class Person {
-    constructor(name = 'Anonymous', location = 'test location') {
-        this.name = name,
-        this.location = location
+    constructor(name = 'Anonymous', age = '28') {
+        this.name = name
+        this.age = age
     }
     getGreeting() {
         return 'Test';
     };
+
+    getDescription() {
+        return `${this.name} is ${this.age} years(s) old`
+    };
 }
 
-const me = new Person('John');
+class Student extends Person {
+    constructor(name, age, major) {
+        super(name, age);
+        this.major = major
+    }
+
+    hasMajor() {
+        return !!this.major;
+    };
+
+    getDescription() {
+        let description = super.getDescription();
+
+        if (this.hasMajor()) {
+            description += `Their major is ${this.major}`;
+        }
+
+        return description;
+    };
+}
+
+class Traveler extends Person {
+    constructor(name, age, homeLocation) {
+        super(name, age);
+        this.homeLocation = homeLocation;
+    }
+
+    getGreeting() {
+        return "Traveler greeting";
+    }
+}
+
+const me = new Student('John', 26, 'Computer Science');
+console.log(me.hasMajor());
+
+const traveler = new Traveler('Dam', 30, 'Teacher');
+console.log(traveler.getGreeting())
+
 
---
+++ indecision-app/playground/jsx-indecision.js
@@ -0,0 +1,61 @@
+const app = {
+    title: 'Indecision App',
+    subtitle: 'Put your life in the hands of computer',
+    options: []
+};
+
+const onFormSubmit = (e) => {
+    e.preventDefault();
+
+    const option = e.target.elements.option.value;
+
+    if (option) {
+        app.options.push(option);
+        e.target.elements.option.value = '';
+    };
+    render();
+};
+
+const onRemoveAll = (e) => {
+    app.options = [];
+    render();
+};
+
+const onMakeDecision = () => {
+    const randomNum = Math.floor(Math.random() * app.options.length);
+    const option = app.options[randomNum];
+    alert(option);
+};
+
+const appRoot = document.getElementById('app');
+
+const render = () => {
+
+    const template = (
+        <div>
+            <h1>{app.title}</h1>
+            {app.subtitle && <p>{app.subtitle}</p>}
+            <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
+            <button disabled={app.options.length === 0} onClick={onMakeDecision}> What should I do?</button>
+            <ol>
+                {
+                    app.options.map((option) => <li key={option}>Options: {option}</li>)
+                }
+            </ol>
+            <form onSubmit={onFormSubmit}>
+                <input type="text" name="option" />
+                <button>Add option</button>
+            </form>
+            <button onClick={onRemoveAll}>Remove all</button>
+        </div>
+    );
+
+    ReactDOM.render(template, appRoot);
+
+};
+
+render();
+
+
+
+
