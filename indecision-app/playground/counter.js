f59022e02a287c1d277c1e4b359ef06c8cb7a9b8
A.Hrytsaienko <temsan89@gmail.com>
2018-01-30 08:51:56 +0200

    counter part 1

--- indecision-app/playground/counter.js
+++ indecision-app/playground/counter.js
@@ -1,42 +1,80 @@
-let count = 0;
+class Counter extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleAddOne = this.handleAddOne.bind(this);
+        this.handleMinusOne = this.handleMinusOne.bind(this);
+        this.handleReset = this.handleReset.bind(this);
+        
+    }
 
-const addOne = () => {
-    count++;
-    showCount();
-    renderCount();
-};
+    handleAddOne(e) {
+        console.log('add one clicked');
+    };
 
-const minusOne = () => {
-    count--;
-    showCount()
-    renderCount();
-};
+    handleMinusOne(e) {
+        console.log('minus one clicked');
+    };
 
-const reset = () => {
-    count = 0;
-    showCount();
-    renderCount();
-};
+    handleReset(e) {
+        console.log('reset clicked');
+    };
 
-const showCount = () => {
-    console.log(`count: ${count}`);
-};
+    render() {
+        return (
+            <div>
+                <h1>Count: </h1>
+                <button onClick={this.handleAddOne}>+1</button>
+                <button onClick={this.handleMinusOne}>-1</button>
+                <button onClick={this.handleReset}>Reset button</button>
+            </div>
+        );
+    }
+}
 
-const btnOne = document.getElementById('btnOne');
+ReactDOM.render(<Counter />, document.getElementById('app'));
 
-const renderCount = () => {
 
-    const templatebtnOne = (
-        <div>
-            <h1>Count: {count}</h1>
-            <button onClick={addOne}>+1</button>
-            <button onClick={minusOne}>-1</button>
-            <button onClick={reset}>Reset button</button>
-        </div>
-    );
 
-    ReactDOM.render(templatebtnOne, btnOne);
 
-};
+// let count = 0;
 
-renderCount();
+// const addOne = () => {
+//     count++;
+//     showCount();
+//     renderCount();
+// };
+
+// const minusOne = () => {
+//     count--;
+//     showCount()
+//     renderCount();
+// };
+
+// const reset = () => {
+//     count = 0;
+//     showCount();
+//     renderCount();
+// };
+
+// const showCount = () => {
+//     console.log(`count: ${count}`);
+// };
+
+// const btnOne = document.getElementById('btnOne');
+
+// const renderCount = () => {
+
+//     const templatebtnOne = (
+//         <div>
+//             <h1>Count: {count}</h1>
+//             <button onClick={addOne}>+1</button>
+//             <button onClick={minusOne}>-1</button>
+//             <button onClick={reset}>Reset button</button>
+//         </div>
+//     );
+
+//     ReactDOM.render(templatebtnOne, btnOne);
+
+// };
+
+// renderCount();   
